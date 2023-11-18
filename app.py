ffef4959fd1859cb2e7100d781763cd4ae9a335d
BrandonTay2001 <brandontay2001@gmail.com>
2023-11-18 21:16:15 +0800

    edited prompt

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from generate_timetable import generate
 
 app = Flask(__name__)
-
-# @app.route('/process_text', methods=['POST'])
-# def process_text():
-#     text = request.form.get('question')  # Get the 'text' parameter from the request
-#     processed_text = get_answer(text)  # Process the input text
-
-#     # Create a response dictionary
-#     response = {
-#         'answer': processed_text
-#     }
-
-#     return jsonify(response)
+CORS(app)
 
 @app.route('/generate_timetable', methods=['POST'])
 def generate_timetable():
-    text = request.json['inputStr']
-    processed_json = generate(text)
+    location = request.json['city']
+    pace = request.json['pace']
+    prompt = request.json['prompt']
+    startDate = request.json['startDate']
+    endDate = request.json['endDate']
+    inputStr = f"Pacing: {pace}\nStart date: {startDate}\nPrompt: {prompt}\nDestination: {location}\nEnd date: {endDate}\n\nResponse (do not include newline characters in response):\n"
+    processed_json = generate(inputStr)
     return jsonify(processed_json)
 
 if __name__ == '__main__':
