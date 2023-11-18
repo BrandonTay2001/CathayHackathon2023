6eca28177ccfb1760299b52bba68502de6463d6f
BrandonTay2001 <brandontay2001@gmail.com>
2023-11-18 18:02:53 +0800

    changed time format

--- timetable-generator/generate_timetable.py
+++ timetable-generator/generate_timetable.py
@@ -6,7 +6,7 @@ openai.api_base = "https://team-21.openai.azure.com/"
 openai.api_version = "2023-09-15-preview"
 openai.api_key = "f9d497ddba0e4f698c4064664785cd54"
 
-timetablePrompt = "You are a trip planner AI. Given a user-given pacing, prompt, destination, start date and number of days, generate a detailed itinerary in the form of a list of JSON objects with the following fields: day, startTime, endTime, attraction\n\nResponse should be in this form:\n[{day: 1, startTime: 1700265600, endTime: 1700269200, attraction: 'Kyoto shrine'}, {day: 1, startTime: 1700276400, endTime: 1700283600, attraction: 'Kyoto museum'}]\n\nRequest:\n"
+timetablePrompt = "You are a trip planner AI. Given a user-given pacing, prompt, destination, start date and number of days, generate a detailed itinerary in the form of a list of JSON objects with the following fields: day, startTime, endTime, attraction\n\nResponse should be in this form:\n[{day: 1, startTime: 2023-12-11T00:00:00.000Z, endTime: 2023-12-14T00:00:00.000Z, attraction: 'Kyoto shrine'}, {day: 1, startTime: 2023-12-25T00:00:00.000Z, endTime: 2023-12-28T00:00:00.000Z, attraction: 'Kyoto museum'}]\n\nRequest:\n"
 
 def generate(inputStr):
   try:
@@ -33,3 +33,13 @@ def generate(inputStr):
   except Exception as e:
     print(e)
     return None
+  
+print(generate("""
+Request: 
+Pacing: Slow
+Start date: 18/11/2023
+Prompt: I want a romantic trip with lots of nature-related sightseeing
+Destination: Kyoto
+Days: 3
+
+Response (do not include newline characters in response):"""))
